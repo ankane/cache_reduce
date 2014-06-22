e90ff0062b5a9b348cba26e961872b19960feabf
Andrew Kane <acekane1@gmail.com>
2014-06-21 18:26:19 -0700

    Use timerange gem

--- lib/cache_reduce.rb
+++ lib/cache_reduce.rb
@@ -35,12 +35,7 @@ module CacheReduce
     end
 
     def fetch(options = {})
-      time_range = options[:time_range]
-
-      hours = [time_range.first.beginning_of_hour]
-      while hour = hours.last + 1.hour and time_range.cover?(hour)
-        hours << hour
-      end
+      hours = TimeRange.new(range: options[:time_range]).expand_start(:hour).step(:hour)
 
       # TODO handle advanced objects as keys
       key = self.key
@@ -75,48 +70,14 @@ module CacheReduce
       result
     end
 
-    # from groupdate
-    def round_time(period, time)
-      time_zone = Time.zone
-      day_start = 0
-      week_start = 6 # sunday
-      time = time.to_time.in_time_zone(time_zone) - day_start.hours
-
-      time =
-        case period
-        when :second
-          time.change(usec: 0)
-        when :minute
-          time.change(sec: 0)
-        when :hour
-          time.change(min: 0)
-        when :day
-          time.beginning_of_day
-        when :week
-          # same logic as MySQL group
-          weekday = (time.wday - 1) % 7
-          (time - ((7 - week_start + weekday) % 7).days).midnight
-        when :month
-          time.beginning_of_month
-        else # year
-          time.beginning_of_year
-        end
-
-      time + day_start.hours
-    end
-
     def by_period(period, options = {})
       result = fetch(options)
       time_range = result.keys.first..result.keys.last
 
-      days = [round_time(period, time_range.first)]
-      while day = days.last + 1.send(period) and time_range.cover?(day)
-        days << day
-      end
-      grouped = result.group_by{|k, v| round_time(period, k) }
+      grouped = result.group_by{|k, v| TimeRange.bucket(period, k) }
 
       final = {}
-      days.each do |day|
+      TimeRange.new(range: time_range).expand_start(:day).step(:day) do |day|
         final[day] = reduce((grouped[day] || []).map(&:last))
       end
       final
